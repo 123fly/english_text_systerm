1825e132a5514c55d8372d760a929e03dfe8bd3d
123fly <762167352@qq.com>
2015-05-29 17:06:24 +0800

    first

--- app/models/score.rb
+++
@@ -1,3 +0,0 @@
-class Score < ActiveRecord::Base
-  validates :number, presence: true
-end
--- db/migrate/20150507015921_add_number_to_scores.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberToScores < ActiveRecord::Migration
-  def change
-    add_column :scores, :number, :string
-  end
-end
